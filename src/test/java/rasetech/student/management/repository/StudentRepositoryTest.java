015476eab826848db4eee60ae51021e9067d20e5
Tomomi Yoshida <yasutmituba576@icloud.com>
2024-11-01 15:05:33 +0900

    スタンダード第52回質問11/1 エラー「Springがdata.sqlファイルをクラスパスから読み込めない」について

--- src/test/java/rasetech/student/management/repository/StudentRepositoryTest.java
+++ src/test/java/rasetech/student/management/repository/StudentRepositoryTest.java
@@ -4,24 +4,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql; // @Sql のインポート
 import org.springframework.transaction.annotation.Transactional;
 import rasetech.student.management.data.Students;
 
 @MybatisTest
 @Transactional
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class) // テストの順序を指定
+@Sql(scripts = "classpath:data.sql") // data.sqlを読み込む
 class StudentRepositoryTest {
 
   @Autowired
   private StudentRepository sut;
 
-
-@Test
-  void 受講生の全件検索が行なえること(){
-  List<Students>actual =sut.search();
-  assertThat(actual.size()).isEqualTo(5);
+  @Test
+  void 受講生の全件検索が行なえること() {
+    List<Students> actual = sut.search();
+    assertThat(actual.size()).isEqualTo(5); // 学生が5人いることを確認
+  }
 }
+
+
+
 //@Test
 //  void 受講生の登録が行なえること(){
 //    Students student = new Students();
@@ -42,4 +50,3 @@ class StudentRepositoryTest {
 //    assertThat(actual.size()).isEqualTo(6);
 //  }
   //リポジトリー登録検索に更新削除
-}
